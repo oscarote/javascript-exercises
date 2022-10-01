8fbcf4574bf3ae7a7c8ac9d83cde548057226c9c
Oscar Otero <oskar.oc@gmail.com>
2022-10-01 12:46:47 +0200

    Create a function that reverses a string
    Created the function and tested it with Jest.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    // const myArray = text.split("");
+    // const myNewArray = myArray.reverse();
+    // text = myNewArray.join("");
+    // return text;
+    // If we put everything together:
+    return text.split("").reverse().join("");
 };
 
 // Do not edit below this line
