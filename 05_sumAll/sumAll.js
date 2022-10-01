333149033bdbd347c0be41785b8a1cc3382b041d
Oscar Otero <oskar.oc@gmail.com>
2022-10-01 17:39:03 +0200

    Write a function that sum all numbers
    Wrote a function that sum all numbers between two different
    numbers, both numbers included. Tested it with Jest.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) !== "number" || typeof(num2) !== "number") return "ERROR";
+    
+    let number1;
+    let number2;
+    
+    if (num1 < num2) {
+        number1 = num1;
+        number2 = num2;
+    } else {
+        number1 = num2;
+        number2 = num1;
+    }
 
+    let sumNumbers = 0;
+
+    for (i = number1; i < number2 + 1; i++) {
+        sumNumbers += i;
+    }
+
+    return sumNumbers;
 };
 
 // Do not edit below this line
