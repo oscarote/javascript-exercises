798c9b39f8e3112945de9fe91af77f64b547cd70
Oscar Otero <oskar.oc@gmail.com>
2022-10-01 17:59:47 +0200

    Create a function to determine a leap year
    Also tested with Jest.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
 
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    // Inicial Solution:
+    // if (year % 100 === 0 && year % 400 === 0) {
+    //     return true;
+    // } else if (year % 4 === 0 && year % 100 !== 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
 
 // Do not edit below this line
