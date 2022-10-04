eba97a774e24cd9dde3b8ec5f99b34ad3600d429
Oscar Otero <oskar.oc@gmail.com>
2022-10-04 20:10:29 +0200

    Create functions as per required

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length ? array.reduce((mult, current) => mult * current) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factNumber = 1;
+  for (let i = 1; i <= number; i++) {
+    factNumber *= i;
+  }
+  return factNumber;
 };
 
 // Do not edit below this line
