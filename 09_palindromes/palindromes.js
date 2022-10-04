751771d7ff9c68525092e3f2b385cae0c1528ed4
Oscar Otero <oskar.oc@gmail.com>
2022-10-04 20:29:55 +0200

    Create function as per required

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    modStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return (modStr.split("").reverse().join("") == modStr);
 };
 
 // Do not edit below this line
