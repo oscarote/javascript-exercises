0406909955ae5e1d344d8007e46cf7b6dfedad10
Oscar Otero <oskar.oc@gmail.com>
2022-10-04 21:31:26 +0200

    Create function as per required

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        return oldestAge < currentAge ? current : oldest;
+    });
+};
 
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
