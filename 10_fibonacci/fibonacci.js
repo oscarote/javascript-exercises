a58ea165f286cb2b6ef47c0092ae956e807c0759
Oscar Otero <oskar.oc@gmail.com>
2022-10-04 20:49:29 +0200

    Create function as per required

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(numb) {
+    if (numb < 0) return "OOPS";
+    let arrayFibo = [0, 1];
+    for (let i = 2; i < numb + 1; i++) {
+        arrayFibo[i] = arrayFibo[i-2] + arrayFibo[i-1];
+    }
+    return arrayFibo[numb];
 };
 
 // Do not edit below this line
